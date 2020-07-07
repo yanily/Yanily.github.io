94b6dbf687537c1bf28e1d455f141aca837d4cfb
Yanily Jimenez <nily_jimenez@hotmail.com>
2020-07-07 16:24:26 -0500

    Body portafolio

--- javascript/main.js
+++ javascript/main.js
@@ -2,3 +2,27 @@ $('.burger').click(function(){
     $('.burger__line').toggleClass("burger__line--open");
     $(".nav__list").toggleClass("nav__list--open");
 });
+var count = $('.trait__container').children().length;
+const size = 180;
+let counter = 1;
+$('#btnLeft').click(function(){
+      if(counter > 1) {
+            --counter;
+            $('.trait').css('transform', 'translateX(' + -size*(counter-1) + '%)');
+      }
+      else {
+            counter = count;
+            $('.trait').css('transform', 'translateX(' + -size*(counter-1) + '%)');
+      }
+});
+
+$('#btnRight').click(function(){
+      if(counter < count) {
+            $('.trait').css('transform', 'translateX(' + -size*counter + '%)');
+            counter++;
+      }
+      else {
+            $('.trait').css('transform', 'translateX(' + 0 + '%)');
+            counter = 1;
+      }
+});
